884fcde550983c7b3bdd7db9093de51c761c494d
bella21 <32242565+bella21@users.noreply.github.com>
2018-01-22 19:45:42 -0600

    Add files via upload

---
+++ start_prog/utils/start_sock_server.bat
@@ -0,0 +1,5 @@
+@echo off 
+E:
+cd E:\workarea\github\ndscheduler3\simple_scheduler\utils
+start python sock_server.py
+exit
---
+++ start_prog/utils/window_name_utils.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+# 获取当前系统显示的窗口pid, handle,window name
+
+
+from ctypes import *
+from win32gui import *
+import pandas as pd
+
+
+user32 = windll.user32
+kernel32 = windll.kernel32
+psapi = windll.psapi
+
+
+# 枚举窗口的回调函数
+def EnumWindowsCallback(hwnd, mouse):
+    pid = c_ulong()
+    # if IsWindow(hwnd) and IsWindowEnabled(hwnd) and IsWindowVisible(hwnd):
+    if IsWindow(hwnd) and IsWindowVisible(hwnd):
+    # if IsWindow(hwnd):
+        user32.GetWindowThreadProcessId(hwnd, byref(pid))
+        caption = GetWindowText(hwnd)
+        if len(caption) == 0:
+            return
+        # print  hex(hwnd), caption.decode('gbk')
+        global handles, pids, captions
+        handles.append(hwnd)
+        pids.append(pid.value)
+        captions.append(caption)
+
+
+# 供外部调用
+def get_window_info():
+    global handles, pids, captions
+    handles = []
+    pids = []
+    captions = []
+    EnumWindows(EnumWindowsCallback, 0) # 枚举所有窗口
+    handle_df = pd.DataFrame(data={'handle': handles, 'pid': pids, 'caption': captions})
+    return handle_df
+
+
+
